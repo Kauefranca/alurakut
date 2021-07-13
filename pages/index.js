a8684c91ee5edde5ae29a5026c7c01d3a5a06e21
Kauefranca <46094226+Kauefranca@users.noreply.github.com>
2021-07-13 01:05:17 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
         <h1 className="title">
           Bem Vindo(a)
         </h1>
-        <OrkutNostalgicIconSet />
+        <OrkutNostalgicIconSet fas=1 confiavel=3 sexy=3/>
       </Box>
       </div>
       <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
